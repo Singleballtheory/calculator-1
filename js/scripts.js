2b1d105d68da060865b32e6a658293562a64cbee
Art Weinstein <artur.weinstein@gmail.com>
2021-08-09 15:06:18 -0700

    add test function for converting C to F.
    Co-authored-by: Tajo Fisher <tajofisher4@gmail.com>
    Scott Hutley <scotthutley1@comcast.net>

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,33 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = divide(number1, number2);
 alert(result);
+
+
+
+function cToF(celsius) {
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
+}
+
+function fToC(fahrenheit) {
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+} 
+cToF(60);
+fToC(45);
+
+
+var l = getElementsByTagName("textArea")[0].innerHTML;
+var g = getElementsByTagName("textArea")[1].innerHTML;
+
+function myFunction() {
+if (g == "") {
+g.innerHTML = l * 3.78541;
+    } else if (l == "") {
+l.innerHTML = g / 3.78541;
+    } 
+}
